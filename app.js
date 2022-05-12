6a64554986500fa98c9612a5a1289ade8c05ad16
Priyanshu Morya <priyanshumaurya.8868@gmail.com>
2022-05-13 04:36:59 +0530

    router basic

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-
+const adminRoute = require('./routes/admin')
+const shopRoute = require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
 
-
-app.use("/add-product", (req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add</button></form>')
-})
-
-app.post("/product",(req,res,next)=>{
- console.log(req.body)
- res.redirect("/")
-})
-
-app.use("/", (req,res,next)=>{
-    res.send('<h1>Hello From Express!</h1>')
-})
+app.use(adminRoute)
+app.use(shopRoute)
 
 
 app.listen(3000)
