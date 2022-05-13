cf15482f97729b2cf1f74f962b84629ed4e19894
Priyanshu Morya <priyanshumaurya.8868@gmail.com>
2022-05-13 10:43:11 +0530

    sendfile of html

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const bodyParser = require('body-parser')
+const { application } = require('express')
 const express = require('express')
 const app = express()
+const path = require('path')
 const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.use("/admin",adminRoute)
 app.use("/",shopRoute)
-
+app.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,'views','404Page.html'))
+})
 
 app.listen(3000)
