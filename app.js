1cc0154daaf577dc15fca0c8a3782bcfc7b5bac3
Priyanshu Morya <priyanshumaurya.8868@gmail.com>
2022-05-18 13:01:33 +0530

    static files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path')
 const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use("/admin",adminRoute)
 app.use("/",shopRoute)
