11003fd16a747dd24911a72a6c1ee147f940c456
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-08-09 21:45:39 +0530

    bug/mongoose_impl

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product({title : title, price: price, description :description, imageUrl :imageUrl, userId : req.user._id});
   product
     .save()
     .then((result) => {
@@ -54,9 +54,11 @@ exports.postEditProduct = (req, res, next) => {
     .then((product) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
+      product.userId = req.user._id;
       (product.imageUrl = updatedImageUrl),
       (product.description = updatedDesc);
-      return this.save();
+
+      return product.save();
     })
     .then((result) => {
       console.log("UPDATED PRODUCT!");
